9fc0e73a710d2cbe84c76e52b1ab624c251b61d0
Brad Grier <bradmgrier@gmail.com>
2025-04-01 08:39:24 -0500

    prevent text scroll to end during open

--- src/main/java/brad/grier/alhena/TextEditor.java
+++ src/main/java/brad/grier/alhena/TextEditor.java
@@ -2,6 +2,7 @@ package brad.grier.alhena;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.io.File;
 
@@ -14,8 +15,9 @@ import javax.swing.JTextPane;
 import com.formdev.flatlaf.util.SystemInfo;
 
 /**
- *  Panel with tabs for a text editor and file chooser. Used for Spartan and Titan uploads.
- * 
+ * Panel with tabs for a text editor and file chooser. Used for Spartan and
+ * Titan uploads.
+ *
  * @author Brad Grier
  */
 public class TextEditor extends JPanel {
@@ -31,8 +33,8 @@ public class TextEditor extends JPanel {
         String fontName = SystemInfo.isWindows ? "Source Code Pro" : "Monospaced";
         Font font = new Font(fontName, Font.PLAIN, 14);
         textArea.setFont(font);
-
-        tabbedPane.addTab("Text", new JScrollPane(textArea));
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        tabbedPane.addTab("Text", scrollPane);
 
         fileChooser = new JFileChooser();
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
@@ -42,14 +44,15 @@ public class TextEditor extends JPanel {
         add(tabbedPane, BorderLayout.CENTER);
         textArea.setText(text);
         setPreferredSize(new Dimension(800, 400));
+        EventQueue.invokeLater(() -> scrollPane.getVerticalScrollBar().setValue(0));
 
     }
 
-    public Object getResult(){
+    public Object getResult() {
         int idx = tabbedPane.getSelectedIndex();
-        if(idx == 0){
+        if (idx == 0) {
             return textArea.getText();
-        }else{
+        } else {
             return fileChooser.getSelectedFile();
         }
     }
