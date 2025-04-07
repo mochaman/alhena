0523ac7f11bbffef7a43220b459933a377ba38c3
Brad Grier <bradmgrier@gmail.com>
2025-04-07 16:46:25 -0500

    improve appearance. hide token for spartan.

--- src/main/java/brad/grier/alhena/TextEditor.java
+++ src/main/java/brad/grier/alhena/TextEditor.java
@@ -7,6 +7,7 @@ import java.awt.Font;
 import java.io.File;
 import java.net.URLEncoder;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -15,6 +16,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
+import com.formdev.flatlaf.ui.FlatTextBorder;
 import com.formdev.flatlaf.util.SystemInfo;
 
 /**
@@ -30,17 +32,21 @@ public class TextEditor extends JPanel {
     private final JTabbedPane tabbedPane;
     private final JTextField tokenField;
 
-    public TextEditor(String text) {
-        setLayout(new BorderLayout(0, 5));
+    public TextEditor(String text, boolean token) {
+        setLayout(new BorderLayout(0, 10));
         tabbedPane = new JTabbedPane();
         textArea = new JTextPane();
         String fontName = SystemInfo.isWindows ? "Source Code Pro" : "Monospaced";
         Font font = new Font(fontName, Font.PLAIN, 14);
         textArea.setFont(font);
         JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         tabbedPane.addTab("Text", scrollPane);
 
         fileChooser = new JFileChooser();
+        fileChooser.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0),
+                BorderFactory.createCompoundBorder(new FlatTextBorder(), BorderFactory.createEmptyBorder(10, 10, 10, 10))));
+        //fileChooser.setBorder(BorderFactory.createCompoundBorder(new FlatTextBorder(), BorderFactory.createEmptyBorder(5,5,5,5)));
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
         fileChooser.setControlButtonsAreShown(false);
 
@@ -52,7 +58,7 @@ public class TextEditor extends JPanel {
         tokenPanel.add(new JLabel("Token:"), BorderLayout.WEST);
         tokenPanel.add(tokenField = new JTextField(), BorderLayout.CENTER);
         add(tokenPanel, BorderLayout.SOUTH);
-
+        tokenPanel.setVisible(token);
         textArea.setText(text);
         setPreferredSize(new Dimension(800, 400));
         EventQueue.invokeLater(() -> scrollPane.getVerticalScrollBar().setValue(0));
