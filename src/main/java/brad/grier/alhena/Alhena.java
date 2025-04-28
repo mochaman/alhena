80826d604b6681ece3c3ec9839e418ace0a2f484
Brad Grier <bradmgrier@gmail.com>
2025-04-28 10:34:02 -0500

    bump version

--- src/main/java/brad/grier/alhena/Alhena.java
+++ src/main/java/brad/grier/alhena/Alhena.java
@@ -125,7 +125,7 @@ public class Alhena {
     private final static List<GeminiFrame> frameList = new ArrayList<>();
     public final static String PROG_NAME = "Alhena";
     public final static String WELCOME_MESSAGE = "Welcome To " + PROG_NAME;
-    public final static String VERSION = "5.0.6";
+    public final static String VERSION = "5.0.7";
     private static volatile boolean interrupted;
     public static final List<String> fileExtensions = List.of(".txt", ".gemini", ".gmi", ".log", ".html", ".pem", ".csv", ".png", ".jpg", ".jpeg", ".mp4", ".mp3", ".ogg", ".opus", ".mov");
     public static final List<String> imageExtensions = List.of(".png", ".jpg", ".jpeg");
