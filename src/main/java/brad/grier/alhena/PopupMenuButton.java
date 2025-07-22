48f6ee1d222e28943e4e1537c395193d713a8127
Brad Grier <bradmgrier@gmail.com>
2025-07-21 22:46:42 -0500

    allow both keyboard and mouse operation

--- src/main/java/brad/grier/alhena/PopupMenuButton.java
+++ src/main/java/brad/grier/alhena/PopupMenuButton.java
@@ -1,6 +1,8 @@
 package brad.grier.alhena;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -16,6 +18,7 @@ import javax.swing.event.PopupMenuListener;
 public class PopupMenuButton extends JButton {
 
     private MouseAdapter ma;
+    private ActionListener al;
     private final Supplier<List<JMenuItem>> menuItemSupplier;
     private final String emptyMessage;
 
@@ -24,14 +27,19 @@ public class PopupMenuButton extends JButton {
         this.menuItemSupplier = menuItemSupplier;
         this.emptyMessage = emptyMessage;
         initializeButton();
+
     }
 
     private void initializeButton() {
 
         ma = new MouseAdapter() {
+
             @Override
             public void mousePressed(MouseEvent e) {
                 if (SwingUtilities.isLeftMouseButton(e)) {
+
+                    removeActionListener(al);
+
                     JPopupMenu popupMenu = new JPopupMenu();
                     PopupMenuListener pml = new PopupMenuListener() {
                         @Override
@@ -52,6 +60,7 @@ public class PopupMenuButton extends JButton {
 
                             EventQueue.invokeLater(() -> {
                                 addMouseListener(ma);
+                                addActionListener(al);
                             });
                         }
 
@@ -68,18 +77,49 @@ public class PopupMenuButton extends JButton {
         };
         // mouse listener instead of action listener to avoid double-triggering
         addMouseListener(ma);
-        // addActionListener(e -> {
 
-        //     JPopupMenu popupMenu = new JPopupMenu();
-        //     List<JMenuItem> mList = menuItemSupplier.get();
-        //     if (mList.isEmpty()) {
-        //         popupMenu.add(new JMenuItem(emptyMessage));
-        //     }
-        //     for (JMenuItem item : mList) {
-        //         popupMenu.add(item);
-        //     }
-        //     popupMenu.show(PopupMenuButton.this, 0, getHeight());
+        al = (ActionEvent e) -> {
+            boolean isKeyboardTriggered = (e.getModifiers() & ActionEvent.MOUSE_EVENT_MASK) == 0;
+            if (!isKeyboardTriggered) {
+                return;
+            }
 
-        // });
+            removeMouseListener(ma);
+            JPopupMenu popupMenu1 = new JPopupMenu();
+
+            PopupMenuListener pml = new PopupMenuListener() {
+                @Override
+                public void popupMenuWillBecomeVisible(PopupMenuEvent pme) {
+                    List<JMenuItem> mList = menuItemSupplier.get();
+                    if (mList.isEmpty()) {
+                        popupMenu1.add(new JMenuItem(emptyMessage));
+                    }
+                    for (JMenuItem item : mList) {
+                        popupMenu1.add(item);
+                    }
+                }
+
+                @Override
+                public void popupMenuWillBecomeInvisible(PopupMenuEvent e) {
+
+                    EventQueue.invokeLater(() -> {
+
+                        addMouseListener(ma);
+                        addActionListener(al);
+                    });
+
+                }
+
+                @Override
+                public void popupMenuCanceled(PopupMenuEvent e) {
+                    
+                }
+            };
+            popupMenu1.addPopupMenuListener(pml);
+            popupMenu1.show(PopupMenuButton.this, 0, getHeight());
+            removeActionListener(al);
+        };
+
+        addActionListener(al);
     }
 }
