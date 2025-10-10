acff5595a18822aae633a5e726eca3fcba6afc88
Brad Grier <bradmgrier@gmail.com>
2025-10-10 13:21:14 -0500

    update to 5.4.1

--- src/main/java/brad/grier/alhena/Alhena.java
+++ src/main/java/brad/grier/alhena/Alhena.java
@@ -149,7 +149,7 @@ public class Alhena {
     private final static List<GeminiFrame> frameList = new ArrayList<>();
     public final static String PROG_NAME = "Alhena";
     public static String welcomeMessage;
-    public final static String VERSION = "5.4.0";
+    public final static String VERSION = "5.4.1";
     private static volatile boolean interrupted;
     // remove vlc extensions and let MimeMapper decide
     public static final List<String> fileExtensions = List.of(".txt", ".gemini", ".gmi", ".log", ".html", ".pem", ".csv", ".png", ".jpg", ".jpeg", ".webp", ".xml", ".json", ".gif", ".bmp", ".md", ".tif", ".svg");
