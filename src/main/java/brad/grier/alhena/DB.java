e445649a33ccead077525362e9e8eacba1e4e14d
Brad Grier <bradmgrier@gmail.com>
2025-10-16 13:08:21 -0500

    pref for socks5 filter

--- src/main/java/brad/grier/alhena/DB.java
+++ src/main/java/brad/grier/alhena/DB.java
@@ -730,6 +730,7 @@ public class DB {
         HashMap<String, String> map = DB.getAllPrefs();
         Alhena.httpProxy = map.getOrDefault("httpproxy", null);
         Alhena.socksProxy = map.getOrDefault("socksproxy", null);
+        Alhena.socksFilter = map.getOrDefault("socksfilter", "false").equals("true");
         Alhena.gopherProxy = map.getOrDefault("gopherproxy", null);
         Alhena.searchUrl = map.getOrDefault("searchurl", null);
         int contentP = Integer.parseInt(map.getOrDefault("contentwidth", "80"));
