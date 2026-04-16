4483014841428a32c28676f830cf7f106cb907a7
Brad Grier <bradmgrier@gmail.com>
2026-04-16 10:46:38 -0500

    fix lingering busy spinner when opening inline http image

--- src/main/java/brad/grier/alhena/Alhena.java
+++ src/main/java/brad/grier/alhena/Alhena.java
@@ -4640,6 +4640,7 @@ public class Alhena {
                                         }
 
                                         p.frame().showGlassPane(false);
+                                        cPage.setBusy(false);
                                     });
                                 });
                             }
